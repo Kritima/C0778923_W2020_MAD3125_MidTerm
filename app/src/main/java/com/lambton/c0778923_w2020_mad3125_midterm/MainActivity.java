3921d4846b4257baf67d11069633b28c97d90074
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-04-07 23:02:26 -0400

    [KRITIMA] Set on click listener added on Birth date

--- app/src/main/java/com/lambton/c0778923_w2020_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/lambton/c0778923_w2020_mad3125_midterm/MainActivity.java
@@ -54,21 +54,10 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        date.setOnClickListener(new View.OnClickListener() {
+        birthDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Calendar cal = Calendar.getInstance();
-                int year = cal.get(Calendar.YEAR);
-                int month = cal.get(Calendar.MONTH);
-                int day = cal.get(Calendar.DAY_OF_MONTH);
-
-                DatePickerDialog dialog = new DatePickerDialog(
-                        FirstActivity.this,
-                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
-                        mDateSetListener,
-                        year, month, day);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                dialog.show();
+                
             }
         });
 
