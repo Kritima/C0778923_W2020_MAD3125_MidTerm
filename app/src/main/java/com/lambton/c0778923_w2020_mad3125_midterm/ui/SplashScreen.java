91f4355f6f73693134e833a995c40f14ccae36b7
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-04-08 23:41:42 -0400

    [KRITIMA] Splash screen activity added

--- app/src/main/java/com/lambton/c0778923_w2020_mad3125_midterm/ui/SplashScreen.java
+++ app/src/main/java/com/lambton/c0778923_w2020_mad3125_midterm/ui/SplashScreen.java
@@ -2,15 +2,31 @@ package com.lambton.c0778923_w2020_mad3125_midterm.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.lambton.c0778923_w2020_mad3125_midterm.R;
 
 public class SplashScreen extends AppCompatActivity {
 
+    public static int SPLASH_TIME_OUT = 5000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
+
+
+
