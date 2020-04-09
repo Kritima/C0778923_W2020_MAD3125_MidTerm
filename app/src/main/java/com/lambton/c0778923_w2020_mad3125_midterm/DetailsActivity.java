bf912a619c0b345bda3e54e86a4f18f4a70387eb
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-04-08 20:49:41 -0400

    [KRITIMA] Recycler View added in Details activity XML

---
+++ app/src/main/java/com/lambton/c0778923_w2020_mad3125_midterm/DetailsActivity.java
@@ -0,0 +1,14 @@
+package com.lambton.c0778923_w2020_mad3125_midterm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
