a7faa9be3c6ca64b5fbbf30a8d549796c2e75d6c
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-04-08 20:23:10 -0400

    [KRITIMA] Gross Income getter added in CRACustomer

--- app/src/main/java/com/lambton/c0778923_w2020_mad3125_midterm/models/CRACustomer.java
+++ app/src/main/java/com/lambton/c0778923_w2020_mad3125_midterm/models/CRACustomer.java
@@ -5,87 +5,87 @@ import java.util.Calendar;
 
 public class CRACustomer {
 
-        private String sin;
-        private String fName;
-        private String lName;
-        private String dob;
-        private String gender;
-        private double grossIncome;
-        private double rrspContributed;
+    private String sin;
+    private String fName;
+    private String lName;
+    private String dob;
+    private String gender;
+    private double grossIncome;
+    private double rrspContributed;
 
-        public String ageKey() {
-            return "Age (Year)";
-        }
+    public String ageKey() {
+        return "Age (Year)";
+    }
 
-        public String birthDateKey() {
-            return "Birth Of Date";
-        }
+    public String birthDateKey() {
+        return "Birth Of Date";
+    }
 
-        public String carryRRSPKey() {
-            return "Carry forward RRSP";
-        }
+    public String carryRRSPKey() {
+        return "Carry forward RRSP";
+    }
 
-        public String cppKey() {
-            return "CPP";
-        }
+    public String cppKey() {
+        return "CPP";
+    }
 
-        public String eiKey() {
-            return "EI";
-        }
+    public String eiKey() {
+        return "EI";
+    }
 
-        public String fedralKey() {
-            return "Federal tax";
-        }
+    public String fedralKey() {
+        return "Federal tax";
+    }
 
-        public String fullNameKey() {
-            return "Full Name";
-        }
+    public String fullNameKey() {
+        return "Full Name";
+    }
 
-        public String genderKey() {
-            return "Gender";
-        }
+    public String genderKey() {
+        return "Gender";
+    }
 
-        public String incomeKey() {
-            return "Gross income";
-        }
+    public String incomeKey() {
+        return "Gross income";
+    }
 
-        public String provinceKey() {
-            return "Provincial Tax (Ontario)";
-        }
+    public String provinceKey() {
+        return "Provincial Tax (Ontario)";
+    }
 
-        public String rrspContributedKey() {
-            return "RRSP Contributed";
-        }
+    public String rrspContributedKey() {
+        return "RRSP Contributed";
+    }
 
-        public String sinKey() {
-            return "Person SIN Number";
-        }
+    public String sinKey() {
+        return "Person SIN Number";
+    }
 
-        public String taxDateKey() {
-            return "Tax Filing date";
-        }
+    public String taxDateKey() {
+        return "Tax Filing date";
+    }
 
-        public String totalPaidTaxKey() {
-            return "Total Tax Payed";
-        }
+    public String totalPaidTaxKey() {
+        return "Total Tax Payed";
+    }
 
-        public String totalPayableTaxKey() {
-            return "Total Taxable Income";
-        }
+    public String totalPayableTaxKey() {
+        return "Total Taxable Income";
+    }
 
-        public CRACustomer(String sin, String fName, String lName, String dob, String gender, double grossIncome, double rrspContributed) {
-            this.sin = sin;
-            this.fName = fName;
-            this.lName = lName;
-            this.dob = dob;
-            this.gender = gender;
-            this.grossIncome = grossIncome;
-            this.rrspContributed = rrspContributed;
-        }
+    public CRACustomer(String sin, String fName, String lName, String dob, String gender, double grossIncome, double rrspContributed) {
+        this.sin = sin;
+        this.fName = fName;
+        this.lName = lName;
+        this.dob = dob;
+        this.gender = gender;
+        this.grossIncome = grossIncome;
+        this.rrspContributed = rrspContributed;
+    }
 
-        public String getSin() {
-            return sin;
-        }
+    public String getSin() {
+        return sin;
+    }
 
     public String fullName() {
         StringBuilder sb = new StringBuilder();
@@ -103,7 +103,12 @@ public class CRACustomer {
         return gender;
     }
 
-
+    public double getGrossIncome() {
+        return grossIncome;
     }
+
+
+
+}
 
 
